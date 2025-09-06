8f8ff163a0f9b252bb7a1396b92f5dc4f600eecb
Breno Corsi <161238146+Breno-Corsi@users.noreply.github.com>
2025-09-06 16:41:36 -0300

    feat(home.html): modal create user 3

--- script/modal.js
+++ script/modal.js
@@ -1,20 +1,26 @@
 const deleteModal = document.getElementById("deletion-box");
-const createModal = document.getElementById("creation-box-1");
+const createModal1 = document.getElementById("creation-box-1");
 const createModal2 = document.getElementById("creation-box-2");
+const createModal3 = document.getElementById("creation-box-3");
 
 deleteModal.onclick = function(event) {
     if (event.target == deleteModal) {
         changeModal("deletion-box", "none");
     }
 }
-createModal.onclick = function(event) {
-    if (event.target == createModal) {
+createModal1.onclick = function(event) {
+    if (event.target == createModal1) {
         changeModal("creation-box-1", "none");
     }
 }
 createModal2.onclick = function(event) {
     if (event.target == createModal2) {
         changeModal("creation-box-2", "none");
+    }
+}
+createModal3.onclick = function(event) {
+    if (event.target == createModal3) {
+        changeModal("creation-box-3", "none");
     }
 }
 
