aea8e3c21507dd6f6f6d59285dcc92198dd073e7
Breno Corsi <161238146+Breno-Corsi@users.noreply.github.com>
2025-09-08 14:44:05 -0300

    feat(index.html): add toggle password visibility button

--- script/login.js
+++ script/login.js
@@ -1,3 +1,18 @@
+const toggleBtn = document.getElementById("toggle-visibility");
+
+toggleBtn.addEventListener("click", () => {
+  toggleBtn.classList.toggle("active");
+});
+
+function togglePasswordVisibility() {
+  var x = document.getElementById("login-password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
 function loginValidation() {
     const loginMail = document.getElementById("login-email").value;
     const loginPassword = document.getElementById("login-password").value;
