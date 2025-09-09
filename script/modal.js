c080d57db44dcbe6218d3f044a7891fcffaea65e
Breno Corsi <161238146+Breno-Corsi@users.noreply.github.com>
2025-09-09 02:13:13 -0300

    feat(modal.js): create account validation steps part 1

--- script/modal.js
+++ script/modal.js
@@ -11,7 +11,6 @@ const allModalID = [
 
 function clearModal() {
     allModalID.forEach(element => {
-        console.log(element);
         let currentModal = document.getElementById(element);
         currentModal.style.display = "none";
     });
@@ -42,3 +41,66 @@ dropZone.addEventListener("drop", e => {
     reader.readAsDataURL(file);
   }
 });
+
+function validateNextButton1() {
+    if (validateBlankInput('input-username') != false) {
+        changeModal('modal-create-account-2', 'flex');
+    }
+}
+
+function validateNextButton2() {
+    if ((validateBlankInput('password-input-1') != false) && (validateBlankInput('password-input-2') != false)) {
+        if(document.getElementById('password-input-1').value != document.getElementById('password-input-2').value) {
+            alert("Both password inputs must be the same!");
+            return false;
+        }
+        changeModal('modal-create-account-3', 'flex');
+    }
+}
+
+function validateNextButton3() {
+    if ((
+        validateBlankInput('full-name-input') != false &&
+        validateBlankInput('cpf-input') != false &&
+        validateBlankInput('sex-input') != false &&
+        validateBlankInput('phone-number-input') != false &&
+        validateBlankInput('birth-date-input') != false &&
+        validateBlankInput('email-input') != false
+    )) {
+        changeModal('creation-confirmation-box', 'flex');
+    }
+}
+
+function validateBlankInput(elementID) {
+    let inputValue = document.getElementById(elementID).value;
+    if (inputValue == "") {
+        alert(elementID + " cannot be empty");
+        return false;
+    }
+}
+
+function validateCPF(cpf) {
+    cpf = cpf.replace(/\D/g,"");
+    cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2");
+    cpf = cpf.replace(/(\d{3})(\d)/,"$1.$2");
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2");
+    inputCPF.value = cpf;
+}
+
+const inputCPF = document.getElementById('cpf-input');
+inputCPF.addEventListener("keyup", (event) => {
+    validateCPF(inputCPF.value);
+})
+
+function validatePhoneNumber(phoneNumber) {
+    phoneNumber = phoneNumber.replace(/\D/g,"");
+    phoneNumber = phoneNumber.replace(/(\d{0})(\d)/,"$1($2");
+    phoneNumber = phoneNumber.replace(/(\d{2})(\d)/,"$1) $2");
+    phoneNumber = phoneNumber.replace(/(\d{5})(\d)/,"$1-$2");
+    inputPhoneNumber.value = phoneNumber;
+}
+
+const inputPhoneNumber = document.getElementById('phone-number-input');
+inputPhoneNumber.addEventListener("keyup", (event) => {
+    validatePhoneNumber(inputPhoneNumber.value);
+})
