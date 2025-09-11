ef2f5c50e2a632f829147ed0dabbddcb948f1252
Breno Corsi <161238146+Breno-Corsi@users.noreply.github.com>
2025-09-11 13:19:41 -0300

    feat(modal.js): register genre input validation

--- script/modal.js
+++ script/modal.js
@@ -193,7 +193,28 @@ function continueRegisterAuthorButton() {
         validateBlankInput('modal-register-author-sex-input') != false &&
         validateBlankInput('modal-register-author-biography-input') != false
     )) {
-            changeModal('creation-confirmation-box', 'flex');
+        changeModal('creation-confirmation-box', 'flex');
+    }
+}
+
+const genreIdInput = document.getElementById('modal-register-genre-id-input');
+document.getElementById('modal-register-genre-id-input').addEventListener("keyup", (event) => {
+    validateGenre(genreIdInput.value);
+})
+
+function validateGenre(genre) {
+    genre = genre.toUpperCase();
+    genre = genre.replace(/[^A-Z]/g,"");
+    genreIdInput.value = genre;
+}
+
+function continueRegisterGenreButton() {
+    if ((
+        validateBlankInput('modal-register-genre-name-input') != false &&
+        validateBlankInput('modal-register-genre-id-input') != false &&
+        validateBlankInput('modal-register-genre-description-input') != false
+    )) {
+        changeModal('creation-confirmation-box', 'flex');   
     }
 }
 
